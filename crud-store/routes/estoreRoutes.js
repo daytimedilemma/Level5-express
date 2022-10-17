4f5678e1e72d16a9276f4e760ce3cef35648678b
Nicholas Miner <nickaminer94@gmail.com>
2022-10-17 16:31:50 -0600

    MVP for crud store

--- crud-store/routes/estoreRoutes.js
+++ crud-store/routes/estoreRoutes.js
@@ -15,6 +15,16 @@ estoreRouter.get("/", (req, res, next) => {
     }) 
 })
 
+estoreRouter.get("/:inventoryId", (req, res, next) => {
+    Inventory.findById({_id: req.params.inventoryId}, (err, foundInventory) => {
+        if(err){
+            res.status(500)
+            return next(err)
+        }
+        return res.status(200).send(foundInventory)
+    })
+})
+
 estoreRouter.post("/", (req, res, next) => {
     const newInventory = new Inventory(req.body)
     newInventory.save((err, savedInventory) => {
@@ -25,6 +35,34 @@ estoreRouter.post("/", (req, res, next) => {
         return res.status(200).send(savedInventory)
     })
 })
+
+estoreRouter.delete("/:inventoryId", (req, res, next) => {
+    Inventory.findByIdAndDelete({_id: req.params.inventoryId}, (err, deletedItem) => {
+        if(err){
+            res.status(500)
+            return next(err)
+        }
+        return res.status(200).send(`Successfully deleted ${deletedItem.name} from the database`)
+    })
+})
+
+estoreRouter.put("/:inventoryId", (req, res, next) => {
+    Inventory.findOneAndUpdate(
+        {_id: req.params.inventoryId},
+        req.body,
+        {new: true},
+        (err, updatedInventory) => {
+            if(err){
+                res.status(500)
+                return next(err)
+            }
+            return res.status(201).send(updatedInventory)
+        }
+    )
+})
+
+
+
 
 
 
